39e3e2afaef6a1dcde9ab80f439fc8740a062380
vedprakash <vedprakash240682@gmail.com>
2026-08-10 16:33:57 +0530

    intro code

---
+++ intro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string FirstName, LastName;
+    cout<<"Enter your first name : ";
+    cin>>FirstName;
+    
+    cout<<"Enter your last name : ";
+    cin>>LastName;
+
+    cout<<"I am "<<FirstName<<" "<<LastName;
+}
